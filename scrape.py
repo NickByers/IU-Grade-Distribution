55063f871ccfe80db4c61fb0659da7a611a8a220
Nick <34692389+NickByers@users.noreply.github.com>
2021-08-31 19:22:17 -0400

    Add files via upload
    Removed column names on downloaded .csv files and merged all files into one large .csv file.

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
 
-
 import requests
+import glob
+import pandas as pd
+import os
 #https://gradedistribution.registrar.indiana.edu/aggregate/export.php?reportID=GrdDistbyClsNbr&term=4212&school=&dept=&rept=1&sesn=&instrname=&subj=&crse=&result=XLS
+
+
+
 
 
 # Took a minute to find the pattern but here it is:
@@ -12,8 +17,8 @@ start = 4212 # value assigned to Spring 2021 (most recent data)
 end = 4118 # oldest data available (Fall 2011)
 
 years = [*range(21, 10, -1)] # 2011 - 2021
-semesters = [8,5,2] # Fall, Sumemer, Spring
-
+semesters = [8,5,2] # Fall, Summer, Spring
+fileNames = []
 for i in range(len(years)):
     for j in range(len(semesters)):
         target = int("4"  + str(years[i]) + str(semesters[j]))
@@ -22,7 +27,19 @@ for i in range(len(years)):
             URL = f"https://gradedistribution.registrar.indiana.edu/aggregate/export.php?reportID=GrdDistbyClsNbr&term={target}&school=&dept=&rept=1&sesn=&instrname=&subj=&crse=&result=XLS"
             print(URL)
             fileName = f"{target}.csv"
+            fileNames.append(fileName)
             newFile = requests.get(URL, allow_redirects=True)
             open(fileName, 'wb').write(newFile.content)
-            #TODO: combine all CSVs into one large CSV
-            
+
+#Removes first two lines from each csv files as these contained column names
+# TODO: Work this into initial write to avoid redundant storage space use
+dataFiles = glob.glob('*.csv')
+for file in dataFiles:
+    lines = open(file).readlines()
+    open(file, 'w')
+
+with open('single.csv', 'w') as single_out:
+    for filename in fileNames:
+         with open(filename) as file_in:
+             all_lines = file_in.readlines()
+             single_out.writelines(all_lines)
